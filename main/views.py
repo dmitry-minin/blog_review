f6859c9fa316f1e08d6940ea95e1540620c79747
Dmitry Minin <d.n.minin@gmail.com>
2025-03-28 20:34:56 +0100

    updated and fixed

--- main/views.py
+++ main/views.py
@@ -8,3 +8,12 @@ def index(request):
         message = request.POST.get('message')
         print(f'You have new message from {name}({email}): {message}')
     return render(request, 'main/index.html')
+
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'You have new message from {name}({email}): {message}')
+    return render(request, 'main/contact.html')
